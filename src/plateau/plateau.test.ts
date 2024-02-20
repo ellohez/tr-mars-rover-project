f746aef6e215f4bb0af98450f488adc7d27313fe
Helen Pickavance <ellohez@gmail.com>
2024-02-20 16:40:58 +0000

    test: first failing test implemented - now passing
    createPlateau returns dimension object

--- src/plateau/plateau.test.ts
+++ src/plateau/plateau.test.ts
@@ -1,7 +1,17 @@
-// import
+import { Dimensions, createPlateau } from "./plateau";
 
 describe("createPlateau", () => {
-  test("should create a 2d array with the given dimensions", () => {});
+  test("should return a dimension with the ", () => {
+    // Arrange
+    const width = 10;
+    const length = 10;
+    // Act
+    const result: Dimensions = createPlateau(width, length);
+    // Assert
+    expect(result).toEqual({ x: width, y: length });
+  });
 
   test("should throw an error if either dimension is < 0", () => {});
+
+  test("should throw an error if both dimensions are 0", () => {});
 });
--- src/plateau/plateau.ts
+++ src/plateau/plateau.ts
@@ -1,1 +1,14 @@
-export const createPlateau = () => {};
+export type Dimensions = {
+  x: number;
+  y: number;
+};
+// Can we create a 2d array - NEEDED?
+// export type Grid = number[][];
+
+export const createPlateau = (width: number, length: number): Dimensions => {
+  if (width <= 0 || length <= 0) {
+    throw new RangeError("Width and length must be greater than zero");
+  }
+
+  return { x: width, y: length };
+};
