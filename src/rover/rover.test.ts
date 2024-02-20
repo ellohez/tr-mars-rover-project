f39e3ebd2097b3fa9f2e7da5ce03d24b270fc630
Helen Pickavance <ellohez@gmail.com>
2024-02-20 21:03:03 +0000

    test: red test for createRover()

--- src/rover/rover.test.ts
+++ src/rover/rover.test.ts
@@ -1,4 +1,5 @@
 import { createPlateau } from "../plateau/plateau";
+import { Orientation, createRover } from "../rover/rover";
 
 describe("createRover function", () => {
   test("should create a rover at the given position", () => {
@@ -8,8 +9,24 @@ describe("createRover function", () => {
     const x = 5;
     const y = 5;
     const plateau = createPlateau(width, length);
+    const direction = "N";
     // Act
-
+    const resultRover = createRover(plateau, x, y, direction);
     // Assert
+    expect(resultRover).toEqual({
+      currentPosition: { x: x, y: y },
+      directionOfTravel: direction,
+    });
+  });
+
+  test("should throw an error when we try and place a rover outside the plateau", () => {
+    const width = 10;
+    const length = 10;
+    const x = 5;
+    const y = 5;
+    const plateau = createPlateau(width, length);
+    const direction = "N";
+    // Act & Assert
+    expect(() => createRover(plateau, x, y, direction)).toThrow(RangeError);
   });
 });
--- src/rover/rover.ts
+++ src/rover/rover.ts
@@ -1,3 +1,29 @@
+import { Plateau, Location, positionOnPlateau } from "../plateau/plateau";
+
+export type CompassPoint = "N" | "E" | "S" | "W";
+export type Orientation = CompassPoint;
+// TODO: Will ned a type guard for CompassPoint/Orientation when UI is added.
+
+export type Rover = {
+  currentPosition: Location;
+  directionOfTravel: Orientation;
+};
+
 // Pass in Plateau, location information and Orientation
 // return Rover object which stores this data.
-export const createRover = () => {};
+export const createRover = (
+  plateau: Plateau,
+  x: number,
+  y: number,
+  direction: Orientation
+): Rover => {
+  // TODO: Error checking
+  // call positionOnPlateau to ensure that the location is within plateau range
+  let position = null;
+  try {
+    position = positionOnPlateau(plateau, x, y);
+  } catch (error) {
+    throw error;
+  }
+  return { currentPosition: position, directionOfTravel: direction };
+};
