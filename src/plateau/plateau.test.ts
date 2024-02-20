19c32fcea0dedaf4c3a9bdf0658d7c0b2eba263c
Helen Pickavance <ellohez@gmail.com>
2024-02-20 17:10:15 +0000

    test: createPlateau tests for zero out of range errors, all passing

--- src/plateau/plateau.test.ts
+++ src/plateau/plateau.test.ts
@@ -11,7 +11,33 @@ describe("createPlateau", () => {
     expect(result).toEqual({ x: width, y: length });
   });
 
-  test("should throw an error if either dimension is < 0", () => {});
+  test("should throw an error if x is 0", () => {
+    // Arrange
+    const width = 0;
+    const length = 10;
+    // Act and Assert
+    expect(() => createPlateau(width, length)).toThrow(
+      new RangeError("Width and length must be greater than zero")
+    );
+  });
 
-  test("should throw an error if both dimensions are 0", () => {});
+  test("should throw an error if y is 0", () => {
+    // Arrange
+    const width = 10;
+    const length = 0;
+    // Act and Assert
+    expect(() => createPlateau(width, length)).toThrow(
+      new RangeError("Width and length must be greater than zero")
+    );
+  });
+
+  test("should throw an error if both dimensions are 0", () => {
+    // Arrange
+    const width = 0;
+    const length = 0;
+    // Act and Assert
+    expect(() => createPlateau(width, length)).toThrow(
+      new RangeError("Width and length must be greater than zero")
+    );
+  });
 });
