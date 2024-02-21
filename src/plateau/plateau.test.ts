66b9bd2fda03af0178cbc44dbfe909deddb760a4
Helen Pickavance <ellohez@gmail.com>
2024-02-21 16:09:40 +0000

    test: rectangular shaped plateau tests

--- src/plateau/plateau.test.ts
+++ src/plateau/plateau.test.ts
@@ -17,6 +17,16 @@ describe("createPlateau", () => {
     expect(result).toEqual({ size: { width: width, height: height } });
   });
 
+  test("should create a regular rectangular shaped plateau and return it", () => {
+    // Arrange
+    const width = 10;
+    const height = 100;
+    // Act
+    const result: Plateau = createPlateau(width, height);
+    // Assert
+    expect(result).toEqual({ size: { width: width, height: height } });
+  });
+
   test("should throw an error if x is 0", () => {
     // Arrange
     const width = 0;
@@ -49,10 +59,20 @@ describe("createPlateau", () => {
 });
 
 describe("positionOnPlateau", () => {
-  test("should return a valid position object if x and y are within boundaries", () => {
+  test("should return a valid position object if x and y are within boundaries (square)", () => {
     // Arrange
     const plateau = createPlateau(100, 100);
     const x = 50;
+    const y = 50;
+    // Act
+    const resultLocation: Location = positionOnPlateau(plateau, x, y);
+    // Assert
+    expect(resultLocation).toEqual({ x: x, y: y });
+  });
+  test("should return a valid position object if x and y are within boundaries (rectangle)", () => {
+    // Arrange
+    const plateau = createPlateau(150, 100);
+    const x = 150;
     const y = 50;
     // Act
     const resultLocation: Location = positionOnPlateau(plateau, x, y);
