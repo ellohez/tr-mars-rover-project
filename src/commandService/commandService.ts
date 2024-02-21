98002b1c4523a7950c21cc1f8d32890f35d6108a
Helen Pickavance <ellohez@gmail.com>
2024-02-21 17:41:17 +0000

    feat: outline of code needed

--- src/commandService/commandService.ts
+++ src/commandService/commandService.ts
@@ -10,3 +10,22 @@ export const isCommand = (
   let testArray = [...CommandsArray.toString()];
   return testArray.includes(input);
 };
+
+export const parseInput = (input: string) => {
+  // determine if input is a plateau setup | rover placement | rover command set
+  // if input is number number -> createPlateau and store
+  // next line of input should be number number Compass point -> createRover and store
+  // next line should be letterletterletter - all valid commands
+  // split instruction string into separate commands
+  // test isCommand - if passes -> processCommand.
+};
+
+export const processOutput = (): string => {
+  // get rover's current position and return as a string.
+  return "";
+};
+
+export const processCommand = (command: string) => {
+  // case L / R -> spinRover
+  // case M -> moveRover
+};
